b6236b50f63d336962cd6c25604764cfb5c33378
Pilz2002 <102512475+Pilz2002@users.noreply.github.com>
2023-01-30 09:55:34 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,238 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+const heading = $("header h2");
+const cdThumb = $(".cd-thumb");
+const audio = $("#audio");
+const cd = $(".cd");
+const playBtn = $(".btn-toggle-play");
+const player = $(".player");
+const progress = $("#progress");
+const nextBtn = $(".btn-next");
+const prevBtn = $(".btn-prev");
+const randomBtn = $(".btn-random");
+const repeatBtn = $(".btn-repeat");
+const playList = $(".playlist");
+const app = {
+  currentIndex: 0,
+  isPlaying: false,
+  isRandom: false,
+  isRepeat: false,
+  songs: [
+    {
+      name: "Pow Get Down",
+      singer: "Raftaar x Fortnite",
+      path: "./music/song1.mp3",
+      image: "https://i.ytimg.com/vi/jTLhQf5KJSc/maxresdefault.jpg",
+    },
+    {
+      name: "Tu Phir Se Aana",
+      singer: "Raftaar x Salim Merchant x Karma",
+      path: "./music/song2.m4a",
+      image:
+        "https://1.bp.blogspot.com/-kX21dGUuTdM/X85ij1SBeEI/AAAAAAAAKK4/feboCtDKkls19cZw3glZWRdJ6J8alCm-gCNcBGAsYHQ/s16000/Tu%2BAana%2BPhir%2BSe%2BRap%2BSong%2BLyrics%2BBy%2BRaftaar.jpg",
+    },
+    {
+      name: "Naachne Ka Shaunq",
+      singer: "Raftaar x Brobha V",
+      path: "./music/song3.m4a",
+      image: "https://i.ytimg.com/vi/QvswgfLDuPg/maxresdefault.jpg",
+    },
+    {
+      name: "Mantoiyat",
+      singer: "Raftaar x Nawazuddin Siddiqui",
+      path: "./music/song4.m4a",
+      image:
+        "https://a10.gaanacdn.com/images/song/39/24225939/crop_480x480_1536749130.jpg",
+    },
+    {
+      name: "Aage Chal",
+      singer: "Raftaar",
+      path: "./music/song5.m4a",
+      image:
+        "https://a10.gaanacdn.com/images/albums/72/3019572/crop_480x480_3019572.jpg",
+    },
+    {
+      name: "Damn",
+      singer: "Raftaar x kr$na",
+      path: "./music/song6.m4a",
+      image:
+        "https://filmisongs.xyz/wp-content/uploads/2020/07/Damn-Song-Raftaar-KrNa.jpg",
+    },
+    {
+      name: "Feeling You",
+      singer: "Raftaar x Harjas",
+      path: "./music/song7.m4a",
+      image:
+        "https://a10.gaanacdn.com/gn_img/albums/YoEWlabzXB/oEWlj5gYKz/size_xxl_1586752323.webp",
+    },
+  ],
+  render: function () {
+    const htmls = this.songs.map((song, index) => {
+      return `
+            <div class="song ${
+              index === this.currentIndex ? "active" : ""
+            }" data-index="${index}">
+            <div class="thumb" style="background-image: url('${song.image}')">
+            </div>
+            <div class="body">
+              <h3 class="title">${song.name}</h3>
+              <p class="author">${song.singer}</p>
+            </div>
+            <div class="option">
+              <i class="fas fa-ellipsis-h"></i>
+            </div>
+          </div>
+            `;
+    });
+    playList.innerHTML = htmls.join("");
+  },
+  defineProperty: function () {
+    Object.defineProperty(this, "currentSong", {
+      get: function () {
+        return this.songs[this.currentIndex];
+      },
+    });
+  },
+  handleEvent: function () {
+    const _this = this;
+    const cdWidth = cd.offsetWidth;
+
+    const cdThumbAnimate = cdThumb.animate([{ transform: "rotate(360deg)" }], {
+      duration: 10000,
+      iterations: Infinity,
+    });
+
+    cdThumbAnimate.pause();
+
+    document.onscroll = function () {
+      const scrollTop = window.scrollY;
+      const newCdWidth = cdWidth - scrollTop;
+      cd.style.width = newCdWidth > 0 ? newCdWidth + "px" : 0;
+      cd.style.opacity = newCdWidth / cdWidth;
+    };
+
+    playBtn.onclick = function () {
+      if (_this.isPlaying) {
+        audio.pause();
+      } else {
+        audio.play();
+      }
+    };
+
+    audio.onplay = function () {
+      _this.isPlaying = true;
+      player.classList.add("playing");
+      cdThumbAnimate.play();
+    };
+
+    audio.onpause = function () {
+      _this.isPlaying = false;
+      player.classList.remove("playing");
+      cdThumbAnimate.pause();
+    };
+
+    audio.ontimeupdate = function () {
+      if (audio.duration) {
+        progress.value = Math.floor((audio.currentTime / audio.duration) * 100);
+      }
+    };
+
+    progress.onchange = function (e) {
+      audio.currentTime = (e.target.value / 100) * audio.duration;
+    };
+
+    nextBtn.onclick = function () {
+      if (_this.isRandom) {
+        _this.playRandom();
+      } else {
+        _this.nextSong();
+      }
+      audio.play();
+      _this.scrollIntoView();
+    };
+
+    prevBtn.onclick = function () {
+      if (_this.isRandom) {
+        _this.playRandom();
+      } else {
+        _this.prevSong();
+      }
+      audio.play();
+      _this.scrollIntoView();
+    };
+
+    randomBtn.onclick = function () {
+      _this.isRandom = !_this.isRandom;
+      randomBtn.classList.toggle("active");
+    };
+    repeatBtn.onclick = function () {
+      _this.isRepeat = !_this.isRepeat;
+      repeatBtn.classList.toggle("active");
+    };
+    audio.onended = function () {
+      if (_this.isRepeat) {
+        audio.play();
+      } else {
+        nextBtn.click();
+      }
+    };
+
+    playList.onclick = function (e) {
+      const songNode = e.target.closest(".song:not(.active)");
+      if (songNode || e.target.closest(".option")) {
+        if (songNode) {
+          _this.currentIndex = Number(songNode.dataset.index);
+          _this.loadCurrentSong();
+          _this.render();
+          audio.play();
+        }
+      }
+    };
+  },
+  loadCurrentSong: function () {
+    heading.textContent = this.currentSong.name;
+    cdThumb.style.backgroundImage = `url('${this.currentSong.image}')`;
+    audio.src = this.currentSong.path;
+  },
+  nextSong: function () {
+    this.currentIndex++;
+    if (this.currentIndex > this.songs.length - 1) {
+      this.currentIndex = 0;
+    }
+    this.loadCurrentSong();
+    this.render();
+  },
+  prevSong: function () {
+    this.currentIndex--;
+    if (this.currentIndex < 0) {
+      this.currentIndex = this.songs.length - 1;
+    }
+    this.loadCurrentSong();
+    this.render();
+  },
+  playRandom: function () {
+    let newCurrentindex;
+    do {
+      newCurrentindex = Math.floor(Math.random() * this.songs.length);
+    } while (newCurrentindex === this.currentIndex);
+    this.currentIndex = newCurrentindex;
+    this.loadCurrentSong();
+  },
+  scrollIntoView: function () {
+    setTimeout(() => {
+      $(".song.active").scrollIntoView({
+        behavior: "smooth",
+        block: "nearest",
+      });
+    }, 1000);
+  },
+
+  start: function () {
+    this.defineProperty();
+    this.handleEvent();
+    this.loadCurrentSong();
+    this.render();
+  },
+};
+
+app.start();
